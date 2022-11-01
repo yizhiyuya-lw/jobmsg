aa7ef93bc07ad855a41450a4e2729cd567382e53
liangw <420915271@qq.com>
2022-11-01 21:53:39 +0800

    项目基础模块代码提交

---
+++ jobmsg-job/job-common/src/main/java/com/yizhiyuya/jobmsg/job/common/model/TriggerParam.java
@@ -0,0 +1,23 @@
+package com.yizhiyuya.jobmsg.job.common.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 任务执行trigger数据封装实体类，用于服务调用
+ * @author lw
+ * @create 2022-11-01-21:46
+ */
+@Data
+@NoArgsConstructor
+public class TriggerParam {
+
+    private Integer jobId; /*任务id*/
+    private Long triggerTime; /*当前任务执行时间毫秒值*/
+    private Long logId; /*日志id，用于回调*/
+    private String executorHandler; /*任务执行handler类名*/
+    private String executorParam; /*任务执行参数*/
+    private Integer executorTimeout; /*任务执行超时时间*/
+    private Integer executorRetryCount; /*任务执行失败重试次数*/
+
+}
